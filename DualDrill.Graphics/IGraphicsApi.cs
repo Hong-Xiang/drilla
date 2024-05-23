70f8aacb2f7acda7faa6ee78a5e1c340519340bd
Hong Xiang <hx.hongxiang@gmail.com>
2024-05-23 08:47:39 +0800

    work in progress

---
+++ DualDrill.Graphics/IGraphicsApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualDrill.Graphics;
+
+public interface IGraphicsApi<TApi, THandle>
+    where TApi : IGraphicsApi<TApi, THandle>
+{
+    internal bool IsValidHandle(THandle handle);
+    internal ValueTask<THandle> CreateInstance();
+    internal ValueTask DestroyInstance(THandle instance);
+}
+
+public sealed class GraphicsApiException(string Message) : Exception(Message)
+{
+}
+
+public static class GraphicsApiExtension
+{
+    public static async ValueTask<GPUInstance<TApi, THandle>> CreateInstance<TApi, THandle>(this TApi api)
+        where TApi : IGraphicsApi<TApi, THandle>
+    {
+        var handle = await api.CreateInstance().ConfigureAwait(false);
+        if (!api.IsValidHandle(handle))
+        {
+            throw new GraphicsApiException("Failed to create instance");
+        }
+        return new GPUInstance<TApi, THandle>(api, handle);
+    }
+}
---
+++ DualDrill.Graphics/UnsafePointer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualDrill.Graphics;
+
+internal unsafe record struct UnsafePointer<T>(T* Value)
+    where T : unmanaged
+{
+}
