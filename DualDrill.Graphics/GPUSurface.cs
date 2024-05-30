e469632b903d0fcab67e32a083338142e7468544
Hong Xiang <hx.hongxiang@gmail.com>
2024-05-30 08:28:27 +0800

    wip

---
+++ DualDrill.Graphics/GPUSurface.cs
@@ -0,0 +1,14 @@
+﻿using DualDrill.Graphics.Native;
+using DualDrill.Graphics.WebGPU.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DualDrill.Graphics;
+
+public sealed class GPUSurface
+{
+    internal NativeHandle<WGPUDisposer, WGPUSurfaceImpl> Handle { get; }
+}
