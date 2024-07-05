be4d8ac58569d1cd4924349c758942ee87ecf70e
Hong Xiang <hx.hongxiang@gmail.com>
2024-07-05 22:37:24 +0800

    webview2 to normal browser page render and push cycle prototype works

--- DualDrill.Server/Components/Pages/WebView2Client.razor.cs
+++ DualDrill.Server/Components/Pages/WebView2Client.razor.cs
@@ -9,6 +9,7 @@ using DualDrill.Server.Services;
 using DualDrill.Server.WebView;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.JSInterop;
 using SIPSorcery.Net;
 using System.Collections.Immutable;
@@ -18,166 +19,32 @@ using System.Reactive.Linq;
 
 namespace DualDrill.Server.Components.Pages;
 
-public partial class WebView2Client : IAsyncDisposable, IDesktopBrowserUI
+public partial class WebView2Client : IAsyncDisposable
 {
     [Inject] ClientStore ClientHub { get; set; } = default!;
-    [Inject] ILogger<DesktopBrowserClient> Logger { get; set; } = default!;
-    [Inject] PeerClientConnectionService ConnectionService { get; set; } = default!;
-    [Inject] FrameSimulationService UpdateService { get; set; } = default!;
-    [Inject] BrowserClient Client { get; set; }
-
-    Task<JSClientModule> ModuleAsync { get; set; }
-    JSClientModule Module { get; set; } = default!;
+    [Inject] ILogger<WebView2Client> Logger { get; set; } = default!;
+    [Inject] IHubContext<DrillHub, IDrillHubClient> HubContext { get; set; } = default!;
+    [Inject] IJSRuntime jsRuntime { get; set; }
+    BrowserClient? Client { get; set; }
+    JSClientModule Module { get; set; } = default;
     [Inject] WebViewService WebViewService { get; set; } = default!;
-
-    private ImmutableArray<Uri> PeerUris { get; set; } = [];
-
-    private readonly CompositeDisposable Subscription = [];
-
-    public string Id => Client.Uri.AbsolutePath;
-    public IClient? PeerClient { get; set; } = null;
-
-
-    bool Connected => PeerClient is not null;
-
-    ElementReference PeerVideoElement { get; set; }
-    ElementReference SelfVideoElement { get; set; }
-
-    ElementReference RenderRootElement { get; set; }
-
     JSRenderService? RenderService { get; set; } = null;
-
-    public void StartRender()
-    {
-    }
-
-    public void StopRender()
-    {
-    }
-
-
-    async Task CreateRenderContext()
-    {
-    }
-
-    Task? ScaleSubscribe { get; set; } = null;
-
-    ElementSize? RootElementSize { get; set; } = null;
-
-    protected override async Task OnInitializedAsync()
-    {
-        //Subscription.Add(ClientHub.Clients.Subscribe(async (clients) =>
-        //{
-        //    Logger.LogInformation("[uri = {ClientUri}] clients update, clients = {Clients}", Client.Uri, string.Join(',', clients.Select(c => c.Uri.ToString())));
-        //    RefreshPeerIds();
-        //    await InvokeAsync(StateHasChanged).ConfigureAwait(false);
-        //}));
-        if (Client is BrowserClient bc)
-        {
-            ModuleAsync = bc.Module.AsTask();
-            bc.UserInterface = this;
-        }
-        await base.OnInitializedAsync().ConfigureAwait(false);
-
-        ScaleSubscribe = SubscribeScale();
-    }
-
-    async Task SubscribeScale()
-    {
-        await foreach (var s in UpdateService.CubeScaleChanges(1.0f, CancellationToken.None))
-        {
-            Console.WriteLine("Scale Changed");
-            await InvokeAsync(StateHasChanged);
-        }
-    }
-
-    public float Scale
-    {
-        get => UpdateService.Scale;
-        set
-        {
-            if (UpdateService.Scale != value)
-            {
-                UpdateService.Scale = value;
-            }
-        }
-    }
-
-    void RefreshPeerIds()
-    {
-        PeerUris = [.. ClientHub.ClientUris.Where(c => c != Client.Uri)];
-    }
-
-    async Task Connect(Uri headsetUri)
-    {
-        var headsetClient = ClientHub.GetClient(headsetUri);
-        if (headsetClient is null || Client is null)
-        {
-            Logger.LogError("Failed to get client for connection");
-            return;
-        }
-        await ConnectionService.SetClients(headsetClient, Client).ConfigureAwait(false);
-    }
-
-    async Task Disconnect()
-    {
-        if (Client is null)
-        {
-            Logger.LogError("Failed to get client for connection");
-            return;
-        }
-        await ConnectionService.ResetClients().ConfigureAwait(false);
-    }
-
-    public async ValueTask DisposeAsync()
-    {
-        Subscription.Dispose();
-    }
-
-
-
-    private async Task SendVideo()
-    {
-        if (PeerClient is null)
-        {
-            Logger.LogError("Can not send video, not connected yet");
-            return;
-        }
-        var cameraStream = await Client.GetCameraStreamAsync().ConfigureAwait(false);
-        await ConnectionService.SendVideo(cameraStream, PeerClient).ConfigureAwait(false);
-        await InvokeAsync(StateHasChanged).ConfigureAwait(false);
-    }
-
-    public async ValueTask SetPeerClient(IClient client)
-    {
-        PeerClient = client;
-        await InvokeAsync(StateHasChanged).ConfigureAwait(false);
-    }
-
-    public async ValueTask RemovePeerClient()
-    {
-        PeerClient = null;
-        await InvokeAsync(StateHasChanged).ConfigureAwait(false);
-    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
         if (firstRender)
         {
-            //await Module.Initialization().ConfigureAwait(false);
-            Module = await ModuleAsync;
-            await using var el = await Module.CreateJSObjectReferenceAsync(RenderRootElement);
-            //RenderService = new(await Client.Module.CreateWebGPURenderServiceAsync());
+            Module = await JSClientModule.CreateAsync(jsRuntime);
+            var connectionId = await Module.GetSignalRConnectionIdAsync();
+            Client = new BrowserClient(jsRuntime, Module, HubContext, connectionId);
+            ClientHub.AddClient(Client);
+            RenderService = new(await Module.CreateHeadlessSharedBufferServerRenderService());
             //RenderService = new(await Client.Module.CreateHeadlessServerRenderService());
-            RenderService = new(await (await Client.Module).CreateHeadlessSharedBufferServerRenderService());
+            //RenderService = new(await (await Client.Module).CreateHeadlessSharedBufferServerRenderService());
             //RenderService = new(await Client.Module.CreateServerRenderPresentService());
             Logger.LogInformation("Blazor render first render called");
-        }
-        if (RenderService is not null)
-        {
-            await RenderService.AttachToElementAsync(RenderRootElement);
-            Logger.LogInformation("Blazor render attach to element called");
+            StateHasChanged();
         }
     }
     public async Task TriggerPushSharedBuffer()
@@ -185,52 +52,7 @@ public partial class WebView2Client : IAsyncDisposable, IDesktopBrowserUI
         await WebViewService.PostSharedBufferAsync(default);
     }
 
-
-
-    public async ValueTask ShowPeerVideo(IMediaStream stream)
+    public async ValueTask DisposeAsync()
     {
-        Console.WriteLine("Show Peer Video");
-        await using var videoElementRef = await Module.CreateJSObjectReferenceAsync(PeerVideoElement).ConfigureAwait(false);
-        await Module.SetVideoElementStreamAsync(videoElementRef, ((JSMediaStreamProxy)stream).Reference);
-    }
-
-    public async ValueTask ShowSelfVideo(IMediaStream stream)
-    {
-        Console.WriteLine("Show Self Video");
-        await using var videoElementRef = await Module.CreateJSObjectReferenceAsync(SelfVideoElement).ConfigureAwait(false);
-        await Module.SetVideoElementStreamAsync(videoElementRef, ((JSMediaStreamProxy)stream).Reference);
-    }
-
-    public async ValueTask ClosePeerVideo()
-    {
-        Console.WriteLine("Close Peer Video");
-        await using var videoElementRef = await Module.CreateJSObjectReferenceAsync(PeerVideoElement).ConfigureAwait(false);
-        var videoProxy = new JsVideoElementProxy(Client, Module, videoElementRef);
-        var mediaStream = await videoProxy.GetStream();
-        var camera = await mediaStream.GetVideoTrack(0);
-        await camera.Stop();
-
-        await Module.RemoveVideoElementStreamAsync(videoElementRef);
-    }
-    public async ValueTask CloseSelfVideo()
-    {
-        Console.WriteLine("Close Self Video");
-        await using var videoElementRef = await Module.CreateJSObjectReferenceAsync(SelfVideoElement).ConfigureAwait(false);
-        var videoProxy = new JsVideoElementProxy(Client, Module, videoElementRef);
-        var mediaStream = await videoProxy.GetStream();
-        var camera = await mediaStream.GetVideoTrack(0);
-        await camera.Stop();
-
-        await Module.RemoveVideoElementStreamAsync(videoElementRef);
-    }
-
-    public async ValueTask<IJSObjectReference> GetCanvasElement()
-    {
-        return await Module.GetProperty<IJSObjectReference>(RenderService.JSRenderContext, "canvas");
-    }
-
-    public ValueTask SetPeerClient(IClient client, RTCPeerConnectionPair pair)
-    {
-        throw new NotImplementedException();
     }
 }
